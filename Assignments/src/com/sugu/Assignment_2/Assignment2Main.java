c75a29fd31a5f1651d37cc7f6ab225e02f0db91b
= <=>
2025-10-04 09:35:31 +0530

    assignment 2 completed

---
+++ Assignments/src/com/sugu/Assignment_2/Assignment2Main.java
@@ -0,0 +1,12 @@
+package com.sugu.Assignment_2;
+
+public class Assignment2Main {
+
+	public static void main(String[] args) {
+		
+		Commission com = new Commission();
+		com.userDetails();	// getting user inputs
+		com.commissionCalculation();	// calculate and print user details with calculated commission
+	}
+
+}
---
+++ Assignments/src/com/sugu/Assignment_2/Student.java
@@ -0,0 +1,10 @@
+package com.sugu.Assignment_2;
+
+public class Student {
+	
+	// Student class with default constructor
+	public Student() {
+		System.out.println("Student object is created");
+	}
+}
+
